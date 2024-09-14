e69480009c75562f289ebe6b75b0b9ab3bf584a2
nirmala-vibgyour <nirmalawrk@gmail.com>
2024-09-14 22:03:25 +0530

    plotting a graph of a sequence

--- graph.f90
+++ graph.f90
@@ -1,3 +1,11 @@
       Program graphPlotting
       !plot different graph of infinite sequences
-      integer::i,j,k
+      integer::i
+      real, dimension(100)::x,y
+      open(unit=1, file="data.txt",status="replace", action="write")
+      do i = 1,100
+        x(i) = i
+        y(i) = (1.0/i)
+        write(1,*)x(i),y(i)
+      enddo
+      endprogram
